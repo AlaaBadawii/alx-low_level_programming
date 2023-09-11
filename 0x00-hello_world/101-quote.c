dc31cfd9407ad85636da1700eeefb587e964eb89
Alaa Ahmed <alaaahmed16112000@gmail.com>
2023-09-11 13:16:04 -0700

    100-intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+/**
+ * main -  C program that prints exactly
+ * and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ * followed by a new line,
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	int len = strlen(str);
+
+	write (1, str, len);
+	write (2, "\n", 1);
+	
+	return (0);
+}
