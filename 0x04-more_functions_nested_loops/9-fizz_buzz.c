a02f4da3e6af450b05168bea91ae9527e8859ae8
Alaa Ahmed <alaaahmed16112000@gmail.com>
2023-09-14 13:47:00 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * main - Fizz-Buzz program
+ * Return: Always 0
+ */
+int main (void)
+{
+	int i;
+	for (i = 0; 1 < 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+
+	printf("\n");
+
+	return (0);
+}
+
+
