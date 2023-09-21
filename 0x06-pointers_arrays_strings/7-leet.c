5cc95dfa1f59ca26e01bddef55fba9a6111f7157
Alaa Ahmed <alaaahmed16112000@gmail.com>
2023-09-21 16:28:19 -0700

    7-1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * leet - function that encodes a string into 1337.
+ * @str: char pointer input.
+ * Return: str.
+ */
+char *leet(char *str)
+{
+	int i, j, e;
+	char *change = "43071";
+	char *values = "aAeEoOtTlL";
+	
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		j = 0;
+		e = 0;
+		while(j < 10)
+		{
+
+			if(str[i] == values[j] || str[i] == values[j + 1])
+			{
+				str[i] = change[e];
+			}
+
+			j += 2;
+			e++;
+		}
+
+		i++;
+	}
+
+	return (str);
+}
+	
+
