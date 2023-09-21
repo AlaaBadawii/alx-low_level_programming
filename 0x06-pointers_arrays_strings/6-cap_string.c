613e4b45ea2f4229078a859594d6a094a80b8d37
Alaa Ahmed <alaaahmed16112000@gmail.com>
2023-09-21 12:01:10 -0700

    6-cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,10 +12,10 @@ char *cap_string(char *str)
 	{
 		if (str[0] > 90)
 			str[0] -= 32;
-		if(str[i] == '\t')
-			*(str + i) = ' ';
+		//if(str[i] == '\t')
+		//	*(str + i) = ' ';
 
-		if (str[i] > 90 && (str[i - 1] == ' ' || str[i - 1] == '\n'))
+		if (str[i] > 90 && ((str[i - 1] == ' ' || str[i - 1] == '\n') ||  str[i - 1] == '\t'))
 			str[i] -= 32;
 
 		i++;
