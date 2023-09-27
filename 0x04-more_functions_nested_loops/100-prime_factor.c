0fe2447cc45ba7fece07b2336c6393ad696b1de0
Alaa Ahmed <alaaahmed16112000@gmail.com>
2023-09-26 17:04:54 -0700

    edit betty style

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,52 +1,39 @@
-#include "main.h"
-/**
- * largest_prime_factor - calculacte the largest prime factor.
- * @num: long long int input
- * Return: prime.
- */
-int largest_prime_factor(long long int num)
-{
-	int prime = 1;
+#include <stdio.h>
+#include <math.h>
 
-	while (num % 2 == 0)
-	{
-		prime = 2;
-
-		num /= 2;
-	}
-
-	for(i = 3; i * i <= num; i += 2)
-	{
-
-		while (num % i == 0)
-		{
-			prime = i;
-
-			num /= i;
-		}
-
-	}
-
-	if (num > 2)
-	{
-		prime = num;
-	}
-	
-	return (prime);
-
+int is_prime(int n) {
+  if (n <= 1) {
+    return 0;
+  }
+  for (int i = 2; i * i <= n; i++) {
+    if (n % i == 0) {
+      return 0;
+    }
+  }
+  return 1;
 }
 
-/**
- * main -  program that finds and prints the largest prime factor.
- * Return: always 0;
- */
-int main(void)
-{
-	int largest_factor;
-
-	largest_factor = largest_prime_factor(612852475143);
-
-	printf("%d", largest_factor);
+int largest_prime_factor(long long int n) {
+  int largest_prime_factor = 1;
+  while (n % 2 == 0) {
+    largest_prime_factor = 2;
+    n /= 2;
+  }
+  for (int i = 3; i * i <= n; i += 2) {
+    while (n % i == 0) {
+      largest_prime_factor = i;
+      n /= i;
+    }
+  }
+  if (n > 2) {
+    largest_prime_factor = n;
+  }
+  return largest_prime_factor;
 }
 
-
+int main() {
+  long long int n = 612852475143;
+  int largest_prime_factor = largest_prime_factor(n);
+  printf("%d\n", largest_prime_factor);
+  return 0;
+}
