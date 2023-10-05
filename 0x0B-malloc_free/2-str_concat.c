8ea2791cca92b3f7f5e8013735f20eb95b256c4c
Alaa Ahmed <alaaahmed16112000@gmail.com>
2023-10-05 10:02:50 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 /**
  * str_concat - that concatenates two strings.
  * @s1: char pointer
@@ -10,42 +9,45 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int i, j, len;
+	unsigned int i, j, k;
 	char *ptr;
 
-	i = 0;
-	j = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-	if (s1 == NULL || s2 == NULL)
-		 return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		;
+	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		;
+	}
 
-	len = strlen(s1) + strlen(s2);
-	ptr = malloc(len * sizeof(char) + 1);
-
+	ptr = malloc(sizeof(char) * (i + j + 1));
 
 	if (ptr == NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	while (*s1 != '\0')
+	for (k = 0; k <= i; k++)
 	{
-		*(ptr + j) = *(s1 + i);
-		s1++;
-		j++;
+		ptr[k] = s1[k];
 	}
 
-	i = 0;
+	j = 0;
 
-	while (*s2 != '\0')
+	while(*s2 != '\0')
 	{
-		*(ptr + j) = *(s2 + i);
-		s2++;
-		i++;
+		ptr[k] = s2[j];
 		j++;
+		k++;
 	}
 
-	*(ptr + len + 1) = '\0';
+	ptr[k + 1] = '\0';
 
 	return (ptr);
 }
