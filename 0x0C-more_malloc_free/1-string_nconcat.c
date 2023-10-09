75e0ce2596bc436279e9a605e4204e411d1a51c4
Alaa Ahmed <alaaahmed16112000@gmail.com>
2023-10-08 17:01:38 -0700

    edit betty style

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,34 +18,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
 	while (s1[s1_size] != '\0')
 	{
 		s1_size++;
 	}
-
 	while (s2[s2_size] != '\0')
 	{
 		s2_size++;
 	}
-
 	if (n > s2_size)
-		n =s2_size;
-
+		n = s2_size;
 	size = s1_size + n + 1;
 	strout = malloc(sizeof(char) * size);
-
 	if (strout == NULL)
 		return (NULL);
-
 	for (i = 0; i < s1_size; i++)
 		strout[i] = s1[i];
-
 	l = 0;
-
 	for (k = i; k < size - 1; k++, l++)
 		strout[k] = s2[l];
-
 	strout[size - 1] = '\0';
 
 	return (strout);
