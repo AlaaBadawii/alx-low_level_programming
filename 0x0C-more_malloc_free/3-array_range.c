17fe3cc0dd333a236952a5dfa0690ec2b80f51ec
Alaa Ahmed <alaaahmed16112000@gmail.com>
2023-10-08 18:04:49 -0700

    check_4

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,7 +12,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	size = max - min + 1;
+	size = max - min + 2;
 
 	ptr = malloc(sizeof(int) * size);
 
@@ -21,7 +21,7 @@ int *array_range(int min, int max)
 		ptr[i] = min + i;
 	}
 
-	ptr[size - 1] = '\0';
+	ptr[size] = '\0';
 
 	return (ptr);
 }
