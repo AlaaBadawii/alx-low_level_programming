0cfee5fa037ab81a80176a2e6d8f8ba480ae879b
Alaa Ahmed <alaaahmed16112000@gmail.com>
2023-10-10 17:29:40 -0700

    all checks done

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef controller
+#define controller
+/**
+ * struct dog - struct dog
+ * @name: member1
+ * @age: member2
+ * @owner: member3
+ *
+ * Description:
+ * name, type = char *
+ * age, type = float
+ * owner, type = char *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* controller */
