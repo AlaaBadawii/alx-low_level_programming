ce82e1739aa219d6bf74267edee96dde991a16e1
Alaa Ahmed <alaaahmed16112000@gmail.com>
2023-10-11 17:23:31 -0700

    :(

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - function that prints a name
+ * @name: char pointer
+ * @f: pointer to function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	printf("%s", name);
+
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,13 @@
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+int _putchar(char c);
+
+#endif /* MY_HEADER_H */
