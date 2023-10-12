8357cf2cd1862ee9252b3c3ec2b254e77fa9830a
Alaa Ahmed <alaaahmed16112000@gmail.com>
2023-10-11 20:02:23 -0700

    h

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+
+/**
+ * get_op_func - pointer function
+ * @s: char pointer input
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] ={
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	}
+	int i;
+
+	while (ops[i].op != s[0])
+		i++;
+	
+	return ops[i].f;
+
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include "function_pointers"
+#include "3-calc"
+
+/**
+ * op_add - calc sum of two numbers
+ * @a: int input
+ * @b: int input
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - calc sub of two numbers
+ * @a: int input
+ * @b: int input
+ * Return: sub
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - calc sub of two numbers
+ * @a: int input
+ * @b: int input
+ * Return: mul
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - calc sub of two numbers
+ * @a: int input
+ * @b: int input
+ * Return: div
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - calc sub of two numbers
+ * @a: int input
+ * @b: int input
+ * Return: mod
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
