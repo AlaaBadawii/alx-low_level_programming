9c3928afb4cf1dd3e0a4795205982896c26c192e
Alaa Ahmed <alaaahmed16112000@gmail.com>
2023-10-20 08:10:25 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,17 @@
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct list_t
+{
+	char *str;
+	int len;
+	struct list_t *next;
+}list_t;
+
+
+size_t print_list(const list_t *h);
+
+#endif /* MY_HEADER_H */
