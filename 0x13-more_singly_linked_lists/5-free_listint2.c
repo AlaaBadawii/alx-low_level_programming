92d3f716d640ff48f5eab0b83825f93c524b213a
Alaa Ahmed <alaaahmed16112000@gmail.com>
2023-10-23 15:33:32 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - function that frees a listint_t list.
+ * @head: listint_t double_pointer
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp, *new;
+
+	new = *head;
+
+	while (new != NULL)
+	{
+		temp = new->next;
+		free(new);
+		new = temp;
+	}
+
+	*head = new;
+}
