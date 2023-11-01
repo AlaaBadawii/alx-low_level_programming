7c19e2d3fad2a5d439a673f633b038ee514a162c
Alaa Ahmed <alaaahmed16112000@gmail.com>
2023-10-31 17:07:26 -0700

    test

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * create_file - function that creates a file.
+ * @filename: file name
+ * @text_content: text_content is a NULL terminated string to write to the file
+ * Return: 1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fdo, fdw, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fdo = open(filename, O_RDONLY | O_CREAT | O_TRUNC, 0600);
+	if (fdo < 0)
+		return (-1);
+
+	while (text_content && *(text_content + len))
+		len++;
+
+	fdw = write(fdo, text_content, len);
+
+	close(fdo);
+
+	if (fdw < 0)
+		return (-1);
+
+	return (1);
+}
